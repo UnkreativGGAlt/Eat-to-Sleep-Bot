a08ec037512cd6ba24d3b985c02656755db2860f
DustinDEV2more <Dustin@Dustin-DM.de>
2020-03-28 13:41:06 +0100

    create votes but it works now

--- commands/vote.js
+++ commands/vote.js
@@ -1,10 +1,3 @@
-// FIX LISTE
-
-// Das Arrey für die antwoten ist ohne count.
-// Das abrechen wenn bereits ein vote für diesen channel exsestiert funktioniert nicht
-// Das Hochladen zur DB funktioniert aber die MSGID wird nicht hinzugefügt
-
-
 module.exports = {
 	name: 'vote',
 	description: 'Erstellt/Beendet einen Vote',
@@ -24,12 +17,14 @@ module.exports = {
             const questionarrey = args.replace(`${config.prefix}vote `, "").replace("start ", ""). replace("create", "").split("|")
             const question = questionarrey[0]
             const awnsers = questionarrey.slice(1)
+            const awnsersq = []
             awnsers.forEach(a => {
-                awnsers[a]= a.count= 0
+                awnsersq.push({awnser: a, count: 0})
             })
+            
+
            var checkvotes = await VOTEDB.find({channel: message.channel.id, open: true})
-                console.log(checkvotes)
-            if (checkvotes.lenth > 0){
+            if (checkvotes.length != 0){
                 message.channel.send(new RichEmbed().setColor(colour.rot).setDescription("In diesem Channel läuft bereits aktuell ein Vote. Bitte beende denn aktuellen und eröffne danach einen neuen"))
                 return;
             }
@@ -39,20 +34,33 @@ module.exports = {
             .setFooter("Du kannst mit den Reaktionen abstimmen!\nDu kannst nur eine Stimme abgeben!\nDu kannst deine Stimme nicht renommieren!")
             
             awnsers.forEach(a => {
-                const number = awnsers.indexOf(a).toString().replace("1", ":two:").replace("2", ":three:").replace("3", ":four:").replace("4", ":five:").replace("5", ":six:").replace("6", ":sevem:").replace("7", ":seven:").replace("8", ":nine:").replace("0", ":one:")
+                const number = awnsers.indexOf(a).toString().replace("1", ":two:").replace("2", ":three:").replace("3", ":four:").replace("4", ":five:").replace("5", ":six:").replace("6", ":seven:").replace("7", ":eight:").replace("8", ":nine:").replace("0", ":one:")
                 embed.setDescription(embed.description + `${number} ${a}: \`0 %\`\n\n`)
             })
 
              var msg = message.channel.send(new RichEmbed().setColor(colour.blau).setDescription("Vote wird erstellt... Bitte warten"))
             message.delete()
 
-            new VOTEDB({q: question, a: awnsers, votet: [], creator: message.author.id, channel: message.channel.id}).save().then(() => {
-                msg.then(m => {
-                    m.edit(embed)
-                    setTimeout(() => {
-                    VOTEDB.findOneAndUpdate({q: question, a: awnsers, votet: [], creator: message.author.id, channel: message.channel.id}, {message: m.id})
-                        
-                    }, 5000);
+            msg.then(msgn => {
+                new VOTEDB({q: question, a: awnsersq, votet: [], creator: message.author.id, channel: message.channel.id, message: msgn.id}).save()
+                .then(() => {
+                msgn.edit(embed).then( async msgr => {
+                   var awsersize = awnsers.length
+                   var zahl = 0
+                   
+                    var reaction_numbers = ["\u0030\u20E3","\u0031\u20E3","\u0032\u20E3","\u0033\u20E3","\u0034\u20E3","\u0035\u20E3", "\u0036\u20E3","\u0037\u20E3","\u0038\u20E3","\u0039\u20E3"]
+                      if (awnsers[0]){await msgn.react(reaction_numbers[1])}
+                      if (awnsers[1]){await msgn.react(reaction_numbers[2])}
+                      if (awnsers[2]){await msgn.react(reaction_numbers[3])}
+                      if (awnsers[3]){await msgn.react(reaction_numbers[4])}
+                      if (awnsers[4]){await msgn.react(reaction_numbers[5])}
+                      if (awnsers[5]){await msgn.react(reaction_numbers[6])}
+                      if (awnsers[6]){await msgn.react(reaction_numbers[7])}
+                      if (awnsers[7]){await msgn.react(reaction_numbers[8])}
+                      if (awnsers[8]){await msgn.react(reaction_numbers[9])}
+                      
+                   
+                })
                 })
             })
 
