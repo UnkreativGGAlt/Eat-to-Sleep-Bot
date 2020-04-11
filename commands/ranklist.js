3c65aea6b9b8f769ab7881d0c8918f3a07db7f0c
DustinDEV2more <Dustin@Dustin-DM.de>
2020-04-11 18:13:35 +0200

    readd ranklist command

--- commands/ranklist.js
+++ commands/ranklist.js
@@ -8,9 +8,36 @@ module.exports = {
         const { RichEmbed } = require('discord.js')
         const colour = require("../colours.json")
 
+        var dbdata = require("../models/MEMBER")
+
+        async function doit(){
+        var rankdata = await dbdata.find().sort({"ranks.rank": -1})
+        var top10 = rankdata.slice(0, 9)
+
+        var Embed = new RichEmbed().setTitle("Top 10 der Member Ränge")
+        .setColor("RANDOM")
+
+        var embeddescription = ""
+        top10.forEach(u => {
+        var name = client.users.get(u.info.id).tag
+        if (rankdata.indexOf(u) == 0){name = "🥇" + name}
+        else if (rankdata.indexOf(u) == 1){name = "🥈" + name}
+        else if (rankdata.indexOf(u) == 2){name = "🥉" + name}
+        else (name = rankdata.indexOf(u) + 1 + `. ${name}`)
+
+        embeddescription = embeddescription + `**${name}**, Level: ${u.ranks.rank}\n`
         
+        })
+        var requestetduser = rankdata.find(u => u.info.id === message.member.id)
+        var ranklistposition = rankdata.indexOf(requestetduser) + 1
 
-        message.channel.send("Aktuell nicht verfügbar")
+        Embed.setDescription(embeddescription)
+        Embed.setFooter(`${message.member.user.tag} ist auf Platz ${ranklistposition}`, message.member.user.avatarURL)
 
+        message.channel.send(Embed)
+
+        }
+        doit()
+        
 	},
 };
