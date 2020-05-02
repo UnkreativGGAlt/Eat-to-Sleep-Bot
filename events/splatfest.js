5ac3504b162310795b954fd558af9b597dfcdd36
DustinDEV2more <Dustin@Dustin-DM.de>
2020-05-02 15:54:12 +0200

    Add splatfest result report

--- events/splatfest.js
+++ events/splatfest.js
@@ -1,5 +1,6 @@
 var fetch = require('node-fetch');
 var channelMclass = require("./channel-management/ChannelM-class")
+const { client, config,} = require("../index")
 var format = require('date-format');
 const { RichEmbed } = require('discord.js')
 
@@ -9,6 +10,8 @@ apiData.then(function (json) {
 var splatfeststart = new Date( json.eu.festivals[0].times.start * 1000)
 var splatfestend = new Date( json.eu.festivals[0].times.end * 1000)
 var splatfestresult = new Date( json.eu.festivals[0].times.result * 1000)
+var schedule = require('node-schedule');
+
 
 var traslatehelp = fetch('https://splatoon2.ink/data/locale/de.json').then(res => res.json());
 traslatehelp.then(function (jsonlang) {
@@ -24,17 +27,53 @@ var Splatfest = new channelMclass(
     .setImage("https://splatoon2.ink/assets/splatnet" + json.eu.festivals[0].images.panel)
     .setThumbnail("https://cdn.wikimg.net/en/splatoonwiki/images/thumb/9/9a/S2_Splatfest_Logo.svg/233px-S2_Splatfest_Logo.svg.png")
     .setTitle("Its Corn.. ähh Splatfest Time!")
-    .setDescription(`Ein neues Splatfest hat begonnen! Das Thema lautet\n**${jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_long}** \n:vs:\n**${jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_long}**`)
+    .setDescription(`<:5010:604756017221468190>Ein neues Splatfest hat begonnen! Das Thema lautet\n**${jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_long}** \n:vs:\n**${jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_long}**`)
     .addField("Zeitplan:", `Splatfest start: ${format.asString('dd.MM hh:mm', splatfeststart)} Uhr\nSplatfest ende: ${format.asString('dd.MM hh:mm', splatfestend)} Uhr\nErgebniss show of: ${format.asString('dd.MM hh:mm', splatfestresult)} Uhr`)
     .addField(`Splatfest-Map:`, `${jsonlang.stages[json.eu.festivals[0].special_stage.id].name}`)
     ,
-    "699621462671097857", //Copie Permission from this Categorie ID
-    "699621462671097857",  //Send Members back to this Categorie ID
+    "585523787408212079", //Copie Permission from this Categorie ID
+    "597176048811245579",  //Send Members back to this Categorie ID
     false, //xp x 2 > true or false
     )
 
     //Post Splatfest Result
     
+    var postresults = schedule.scheduleJob(splatfestresult, function(){setTimeout(() => {
+         var splatfestid = json.eu.festivals[0].festival_id
 
-})
+        var splatfestresults = json.eu.results.find(s => s.festival_id === splatfestid)
+
+        function prozentrechner(a ,b) {
+            var Grundwert = a + b
+            var Prozentwert = a
+
+            var Ergebniss = Prozentwert * 100 / Grundwert
+            if ( Ergebniss > b * 100 / Grundwert) return `**${Math.round(Prozentwert * 100 / Grundwert)}%**`
+            else if ( Ergebniss < b * 100 / Grundwert) return `${Math.round(Prozentwert * 100 / Grundwert)}%`
+            else if ( Ergebniss == b * 100 / Grundwert) return `**${Math.round(Prozentwert * 100 / Grundwert)}**%`
+        }
+
+var stimmenA = prozentrechner(splatfestresults.rates.vote.alpha, splatfestresults.rates.vote.bravo)
+var stimmenB = prozentrechner(splatfestresults.rates.vote.bravo, splatfestresults.rates.vote.alpha)
+var Standart_KampfA = prozentrechner(splatfestresults.rates.regular.alpha, splatfestresults.rates.regular.bravo)
+var Standart_KampfB = prozentrechner(splatfestresults.rates.regular.bravo, splatfestresults.rates.regular.alpha)
+var Profi_KampfA = prozentrechner(splatfestresults.rates.challenge.alpha, splatfestresults.rates.challenge.bravo)
+var Profi_KampfB = prozentrechner(splatfestresults.rates.challenge.bravo, splatfestresults.rates.challenge.alpha)
+
+var names = [jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_short, jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_short, "2", "3", jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_short, jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_short, "6", "7"]
+        client.channels.get("586177035278483466").send(
+            new RichEmbed()
+            .setTitle("DIE SPLATFEST ERGEBNISSE SIND DA!").setColor("RANDOM")
+            .setThumbnail("https://splatoon2.ink/assets/splatnet" + json.eu.festivals[0].images.panel)
+            .setFooter("Klicke auf die dunklen Felder um sie sichbar zu machen", "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/9/9a/S2_Splatfest_Logo.svg/233px-S2_Splatfest_Logo.svg.png")
+            .setDescription("<:5010:604756017221468190>Die Ergebnisse des Splatfestes wurden mir gerade vom Miezrichter vorbeigebracht. Schauen wir mal wer gewonnen hat:")
+            .addField("Stimmen:", `${jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_short} >  ||${stimmenA} VS ${stimmenB}|| < ${jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_short}`)
+            .addField("Standart Kampf:", `${jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_short} >  ||${Standart_KampfA} VS ${Standart_KampfB}|| < ${jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_short}`)
+            .addField("Profi Kampf:", `${jsonlang.festivals[json.eu.festivals[0].festival_id].names.alpha_short} >  ||${Profi_KampfA} VS ${Profi_KampfB}|| < ${jsonlang.festivals[json.eu.festivals[0].festival_id].names.bravo_short}`)
+            .addField("Splatfest Gewinner:", `UND DAS GEWINNERTEAM DES SPLATFESTS LAUTET\n ||**TEAM ${names[splatfestresults.summary.total]}**\nHERZLICHEN GLÜCKWUNSCH!\nSorry an alle von Team ${names[splatfestresults.summary.total + 4]}||`)
+        )
+
+     })
+
+    }, 30000)})
 })
