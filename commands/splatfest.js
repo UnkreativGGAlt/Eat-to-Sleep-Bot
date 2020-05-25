147f1ea2caac8888867d21cacd592a57dfce4393
DustinDEV2more <Dustin@Dustin-DM.de>
2020-05-25 16:25:14 +0200

    Add _splatfest command

--- commands/splatfest.js
+++ commands/splatfest.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'splatfest',
-	description: 'Just a little dev command',
-    usage: `splatfest [id]` ,
+	description: 'Mit diesem Command könnt ich euch die Ergebnisse des aktuellen Splatoon 2 Splatfestes anzeigen lassen (falls vorhanden)',
+    usage: `splatfest` ,
 	execute(message, args) {
        
 
@@ -9,21 +9,38 @@ module.exports = {
         const { RichEmbed } = require('discord.js')
         const colour = require("../colours.json")
         const Discord = require('discord.js');
+        const fetch = require("node-fetch")
 
         const puppeteer = require('puppeteer');
 
         const viewport = {
-            width: 1012,
-            height: 600,
+            width: 720,
+            height: 1000,
             deviceScaleFactor: 2,
         };
-        
 
+        var apiData = fetch('https://splatoon2.ink/data/festivals.json').then(res => res.json());
+apiData.then(function (json) {
         (async () => {
             const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
             const page = await browser.newPage();
             page.setViewport(viewport)
-            await page.goto("http://localhost:6677/splatfest/" + args[0], {
+
+            await page.setRequestInterception(true);
+            
+            page.on('request', request => {
+                // Do nothing in case of non-navigation requests.
+                if (!request.isNavigationRequest()) {
+                  request.continue();
+                  return;
+                }
+                // Add a new header for navigation request.
+                const headers = request.headers();
+                headers["cookie"] = `iksm_session=${config.tokens.nintendo}; lang=de-DE;`;
+                request.continue({ headers });
+              });
+            
+            await page.goto("https://app.splatoon2.nintendo.net/records/festival/" + json.eu.festivals[0].festival_id, {
                 waitUntil: 'networkidle0', // Wait until the network is idle
             });
             var screenshot = await page.screenshot();
@@ -33,6 +50,6 @@ module.exports = {
             await browser.close();
           })();
         
-       
+        })
 	},
 };
