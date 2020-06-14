da9dbf2482deaf2cfc6c88e37ac2539188c4c491
DustinDEV2more <Dustin@Dustin-DM.de>
2020-06-14 15:05:26 +0200

    Update Music command

--- commands/music.js
+++ commands/music.js
@@ -3,7 +3,6 @@ module.exports = {
 	description: 'Spielt Musik über den Bot ab',
   usage: `m (play <yt-link> | stop | skip)` ,
 	async execute(message, args) {
-    if (message.member.id != "330380702505762817") return message.reply("Sorry. Aktuell kann nur Dustin diesen Command benutzten. Er wird bald von 'Ehrenmenschen' und 'WG-Mitgliedern' benutzbar sein")
 
         const { client, config} = require('../index')
         const { RichEmbed } = require('discord.js')
@@ -14,12 +13,21 @@ module.exports = {
         if (args[0] == "play"){
 
           if (!args[1]) return message.channel.send(new RichEmbed().setColor(colour.rot).setDescription("Du musst einen Youtube Link angeben"));
+          if (message.member.voiceChannel == null) return message.channel.send(new RichEmbed().setColor(colour.rot).setDescription("Du befindest dich nicht in einem Voicechhannel"));
           
+          var Videotitel = "das Video"
+         await ytdl.getInfo(args[1], (err, info) => {
+            if (err) throw err;
+            Videotitel = `[${info.title}](${args[1]})`
+          });
+
           if (client.music[message.guild.id] && client.music[message.guild.id] != null){
             client.music[message.guild.id].queue.push(args[1])
+            message.channel.send(new RichEmbed().setColor(colour.rot).setDescription(`Ich habe ${Videotitel} zur queue hinzugefügt`));
           }
           else {
             client.music[message.guild.id]= {queue: [args[1]]}
+            message.channel.send(new RichEmbed().setColor(colour.rot).setDescription(`Ich werde ${Videotitel} in deinem Voicechannel abspielen`));
 
             play(message.guild.id)
           }
@@ -56,16 +64,27 @@ module.exports = {
         if (args[0] == "skip"){
           if (client.music[message.guild.id] && client.music[message.guild.id] != null){
             client.music[message.guild.id].dispatcher.destroy();
+            new RichEmbed().setColor(colour.grün).setDescription("Der Song wurde übersprungen");
+
           }
-          else return message.channel.send(new RichEmbed().setColor(colour.rot).setDescription("Es kann kein Song geskippt werden wenn kein Song läuft"));
+          else return message.channel.send(
+            new RichEmbed().setColor(colour.rot).setDescription("Es kann kein Song geskippt werden wenn kein Song läuft"));
           }
 
           if (args[0] == "stop"){
             if (client.music[message.guild.id] && client.music[message.guild.id] != null){
               client.music[message.guild.id].queue = [];
               client.music[message.guild.id].dispatcher.destroy();
-            }
+            new RichEmbed().setColor(colour.grün).setDescription("Die Wiedergabe wurde gestoppt");
+          }
             else return message.channel.send(new RichEmbed().setColor(colour.rot).setDescription("Du kannst nichts stoppen werden wenn kein Song läuft"));
+            }
+
+          if (args[0].toLowerCase() == "queue"){
+            if (client.music[message.guild.id] && client.music[message.guild.id] != null){
+              
+          }
+            else return message.channel.send(new RichEmbed().setColor(colour.rot).setDescription("Du kannst dir die Queue nicht anzeigen lassen"));
             }
 
         
